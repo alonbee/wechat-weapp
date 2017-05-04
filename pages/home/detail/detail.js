3c23eb672cc6eaffac2b76193c52412f75a8b04c
岁暮少年玻璃心 <yujunren1994@163.com>
2017-05-04 16:11:11 +0800

    首页卡片收藏和分享功能更新

--- pages/home/detail/detail.js
+++ pages/home/detail/detail.js
@@ -8,91 +8,19 @@ Page({
     detail: {}
   },
   onLoad:function(options){
-    var that = this;
-
-    // 发起请求，拿到数据
-    api.getVolDetailById({
+    api.getHpDetailById({
      query: {
        id: options.id
      },
-     success: function(res) {
+     success: (res) => {
       if (res.data.res === 0) {
-        var detail = res.data.data;
-        detail.hp_makettime = util.formatMakettime(detail.hp_makettime);
+        let detail = res.data.data;
+        detail.hp_makettime = util.formatHpMakettime(detail.hp_makettime);
+        detail.hp_author = util.formatHpAuthor(detail.hp_author);
+        detail.hp_content = util.formatHpContent(detail.hp_content);
         that.setData({ detail });
       }
      }
-    });
-
-    // 设置本地存储，实现收藏
-    var volsIsFavorite = wx.getStorageSync('vols_is_favorite');
-    var volId = options.id;
-    // 将当前的卡券信息绑定在data上，方便后面使用
-    this.data.currentVolId = volId;
-    if (volsIsFavorite) {
-      var isFavorite = volsIsFavorite[volId];
-      this.setData({
-        isFavorite: isFavorite
-      });
-    } else {
-      var volsIsFavorite = {};
-      volsIsFavorite[volId] = false;
-      wx.setStorageSync('vols_is_favorite', volsIsFavorite);
-    }
-
-  },
-  // 点击收藏事件
-  onFavoriteTap: function() {
-    this.getVolsIsFavoriteAsy();
-  },
-  // 取得所有的卡片是否收藏信息
-  getVolsIsFavoriteAsy: function() {
-    var that = this;
-    wx.getStorage({
-      key: 'vols_is_favorite',
-      success: function(res){
-        var volsIsFavorite = res.data;
-        var isFavorite = volsIsFavorite[that.data.currentVolId];
-        // 取反设置收藏
-        isFavorite = !isFavorite;
-        volsIsFavorite[that.data.currentVolId] = isFavorite;
-        that.showToast(volsIsFavorite, isFavorite);
-      }
-    })
-  },
-  // 设置和显示收藏信息
-  showToast: function(volsIsFavorite, isFavorite) {
-    // 更新收藏信息
-    wx.setStorageSync('vols_is_favorite', volsIsFavorite);
-    // 更新数据
-    this.setData({
-      isFavorite: isFavorite
-    });
-    wx.showToast({
-      title: isFavorite? "收藏成功" : "取消收藏",
-      duration: 1000,
-      icon: "success"
-    });
-  },
-  // 点击分享事件
-  onShareTap: function() {
-    var itemList = [
-      "分享给好友",
-      "分享到朋友圈",
-      "分享到QQ空间",
-      "分享到微博"
-    ]
-    // 操作菜单栏
-    wx.showActionSheet({
-      itemList: itemList,
-      itemColor: "#666",
-      success: function(res) {
-        // 显示模态弹窗
-        wx.showModal({
-          title: "这是分享的标题",
-          content: "小程序还没有内容分享功能!"
-        });
-      }
     });
   },
   // 页面分享
--- pages/home/home.js
+++ pages/home/home.js
@@ -34,6 +34,16 @@ Page({
             hp.hp_makettime = util.formatHpMakettime(hp.hp_makettime);
             hp.hp_author = util.formatHpAuthor(hp.hp_author);
             hp.hp_content = util.formatHpContent(hp.hp_content);
+            // 设置本地存储，实现收藏
+            let hpsIsFavorite = wx.getStorageSync('hps_is_favorite') || {};
+            if (hpsIsFavorite[hp.hpcontent_id]) {
+              let isFavorite = hpsIsFavorite[hp.hpcontent_id];
+              hp.is_favorite = isFavorite;
+            } else {
+              hpsIsFavorite[hp.hpcontent_id] = false;
+              hp.is_favorite = false;
+              wx.setStorageSync('hps_is_favorite', hpsIsFavorite);
+            }
             hps.push(hp);
           }
           this.getHps(idList);
@@ -68,5 +78,63 @@ Page({
         }
       });
     }
-  }
+  },
+  // 点击收藏事件
+  onFavoriteTap: function (event) {
+    let hpId = event.currentTarget.dataset.hpId;
+    this.getHpsIsFavoriteAsy(hpId);
+  },
+  // 取得所有的卡片是否收藏信息
+  getHpsIsFavoriteAsy: function(hpId) {
+    wx.getStorage({
+      key: 'hps_is_favorite',
+      success: (res) => {
+        let hpsIsFavorite = res.data;
+        let isFavorite = hpsIsFavorite[hpId];
+        // 取反设置收藏
+        isFavorite = !isFavorite;
+        hpsIsFavorite[hpId] = isFavorite;
+        this.showToast(hpsIsFavorite, isFavorite, hpId);
+      }
+    });
+  },
+  // 设置和显示收藏信息
+  showToast: function (hpsIsFavorite, isFavorite, hpId) {
+    // 更新收藏信息
+    wx.setStorageSync('hps_is_favorite', hpsIsFavorite);
+    // 更新数据
+    let hps = this.data.hps;
+    for (let i = 0; i < hps.length; i++) {
+      if (hps[i].hpcontent_id === hpId) {
+        hps[i].is_favorite = isFavorite;
+      }
+    }
+    this.setData({ hps });
+    wx.showToast({
+      title: isFavorite? "收藏成功" : "取消收藏",
+      duration: 1000,
+      icon: "success"
+    });
+  },
+  // 点击分享事件
+  onShareTap: function() {
+    var itemList = [
+      "分享给好友",
+      "分享到朋友圈",
+      "分享到QQ空间",
+      "分享到微博"
+    ]
+    // 操作菜单栏
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor: "#666",
+      success: function(res) {
+        // 显示模态弹窗
+        wx.showModal({
+          title: "这是分享的标题",
+          content: "小程序还没有内容分享功能!"
+        });
+      }
+    });
+  },
 })
