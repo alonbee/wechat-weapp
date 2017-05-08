71b9ba2d33f4520d136db9c3006ab3be24bfe9c8
岁暮少年玻璃心 <yujunren1994@163.com>
2017-05-08 15:27:25 +0800

    音乐模块详情页完成

--- pages/music/detail/detail.js
+++ pages/music/detail/detail.js
@@ -1,10 +1,24 @@
 // pages/music/detail/detail.js
+let api = require('../../../api/api.js');
+let util = require('../../../util/util.js');
+
 Page({
   data:{
     music: {}
   },
   // 页面加载
-  onLoad: function(options) {
-    console.log(options);
+  onLoad: function (options) {
+    api.getMuiscById({
+      query: {
+        id: options.id
+      },
+      success: (res) => {
+        if (res.data.res === 0) {
+          let music = res.data.data;
+          music.story = util.filterContent(music.story);
+          this.setData({ music });
+        }
+      }
+    });
   }
 })
